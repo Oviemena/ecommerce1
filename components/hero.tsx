97d2be33c71bee9782255faffb9134835d4af29d
Oviemena Okpowodu <okpowoduoviemena999@gmail.com>
2024-06-08 22:05:29 +0100

    change iphone name

--- components/hero.tsx
+++ components/hero.tsx
@@ -93,7 +93,7 @@ const Hero = () => {
     <section className="w-full nav-height bg-black relative">
       <div className="h-5/6 w-full flex-center flex-col">
         <p id="hero" className="hero-title">
-          iPhone 15 Pro
+          iPhone 14 Pro
         </p>
         <div className="w-full md:h-3/4">
           <Video src={videoSrc} className="pointer-events-none" autoPlay muted playsInline controls={false} placeholder="none"/>
